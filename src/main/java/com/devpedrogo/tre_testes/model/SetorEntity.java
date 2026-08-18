f5bc983df9ca48fb595f0178d40c3d640f699b22
Pedro Gouveia <pedrohenriquecombr14@gmail.com>
2026-08-18 20:47:44 -0300

    cria entidade setor com seus atributos

---
+++ src/main/java/com/devpedrogo/tre_testes/model/SetorEntity.java
@@ -0,0 +1,18 @@
+package com.devpedrogo.tre_testes.model;
+
+import java.math.BigDecimal;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class SetorEntity {
+    private BigDecimal codigoSetor;
+    private String nomeSetor;
+    private String siglaSetor;
+}
